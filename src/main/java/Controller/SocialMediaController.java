aed82c859870a84e786064f791e57b3619de5876
Tania Soutonglang <tsoutonglang12@gmail.com>
2024-12-25 06:22:04 +0000

    Added to the controller

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,10 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
 import Service.AccountService;
 import Service.MessageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -39,8 +42,18 @@ MessageService messageService;
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
-        app.get("/register", this::registerUserHandler);
-        app.get("/login", this::loginUserHandler);
+        app.post("/register", this::postAccountRegHandler);
+        app.post("/login", this::postAccountLoginUserHandler);
+
+        app.post("/messages", this::postMessageHandler);
+        app.get("/messages", this::getMessageHandler);
+
+        app.delete("/messages", this::deleteMessage);
+
+        app.patch("/messages", this::editMessageHandler);
+
+        app.get("messages", this::getMessageUserHandler);
+
         app.start(8000);
 
         return app;
@@ -52,6 +65,34 @@ MessageService messageService;
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    /**
+     * Handler to post a new user.
+     * The registration will be successful if and only if the username is not blank, the password is at least 4 characters long, 
+     * and an Account with that username does not already exist. If all these conditions are met, the response body should contain a 
+     * JSON of the Account, including its account_id. 
+     * The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+     * If the registration is not successful, the response status should be 400. (Client error)
+     * @param ctx The Javalin Context object manages information about both the HTTP request and response.
+     * @throws JsonProcessingException will be thrown if there is an issue
+     */
+    private void postAccountRegHandler(Context ctx) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        // create an Account object
+        Account account = mapper.readValue(ctx.body(), Account.class);
+
+        // call the service method to register the account
+        Account addedAccount = accountService.addedAccount(account);
+
+        // check if the registration was successful
+        if(addedAccount == null){
+            ctx.status(400);
+        } else{
+            ctx.status(200);
+            ctx.json(mapper.writeValueAsString(addedAccount));
+        }
     }
 
 
